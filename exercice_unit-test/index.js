b6e4844595b2d05e2ded73fcfdd0fce9744cac3b
AnthonyDWeb <delforge.anthony@gmail.com>
2021-09-21 13:55:23 +0200

    add format date test

--- exercice_unit-test/index.js
+++ exercice_unit-test/index.js
@@ -1,5 +1,18 @@
 const evenOrNot = (num) =>{ return (num % 2 === 0 || num % 2 === -0) };
 
+const formatDate = (date) => {
+    var dateTest = new Date(date);
+    var month = dateTest .getMonth() + 1;
+    var day = dateTest .getDate();
+    var year = dateTest .getFullYear();
+
+    if(month < 10){
+        month = "0"+month;
+    }
+    return `${day}/${month}/${year}`;
+}
+
 module.exports = {
-    evenOrNot
+    evenOrNot,
+    formatDate
 }
